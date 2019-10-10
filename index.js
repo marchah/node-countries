cf5c4221900b8482aab15590a010cb0ab632fc82
marchah <marchah.dev@gmail.com>
2019-10-10 12:56:52 -0700

    fix compatibility EI11

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-'use strict';
+"use strict";
 
-const _ = require('lodash');
+const _ = require("lodash");
 
-const countries = require('./data/countries.json');
+const countries = require("./data/countries.json");
 
 const toExport = {
-  JSON: countries,
+  JSON: countries
 };
-
 
 /**
  * Find the country object of the given country name
@@ -19,10 +18,14 @@ const toExport = {
 function getCountryByName(name, useAlias) {
   if (!_.isString(name)) return undefined;
 
-  return _.find(countries, function (country) {
+  return _.find(countries, function(country) {
     if (useAlias) {
-      return country.name.toUpperCase() === name.toUpperCase()
-        ||  _.find(country.alias, function (alias) (alias.toUpperCase() === name.toUpperCase()));
+      return (
+        country.name.toUpperCase() === name.toUpperCase() ||
+        _.find(country.alias, function(alias) {
+          return alias.toUpperCase() === name.toUpperCase();
+        })
+      );
     }
     return country.name.toUpperCase() === name.toUpperCase();
   });
@@ -38,13 +41,20 @@ function getCountryByName(name, useAlias) {
 function getCountryByNameOrShortName(name, useAlias) {
   if (!_.isString(name)) return undefined;
 
-  return _.find(countries, function (country) {
+  return _.find(countries, function(country) {
     if (useAlias) {
-      return country.name.toUpperCase() === name.toUpperCase()
-        || country.alpha2.toUpperCase() === name.toUpperCase()
-        ||  _.find(country.alias, function (alias) (alias.toUpperCase() === name.toUpperCase()));
+      return (
+        country.name.toUpperCase() === name.toUpperCase() ||
+        country.alpha2.toUpperCase() === name.toUpperCase() ||
+        _.find(country.alias, function(alias) {
+          return alias.toUpperCase() === name.toUpperCase();
+        })
+      );
     }
-    return country.name.toUpperCase() === name.toUpperCase() || country.alpha2.toUpperCase() === name.toUpperCase();
+    return (
+      country.name.toUpperCase() === name.toUpperCase() ||
+      country.alpha2.toUpperCase() === name.toUpperCase()
+    );
   });
 }
 
@@ -52,7 +62,6 @@ toExport.getCountryByName = getCountryByName;
 toExport.findCountryByName = getCountryByName;
 toExport.getCountryByNameOrShortName = getCountryByNameOrShortName;
 toExport.findCountryByNameOrShortName = getCountryByNameOrShortName;
-
 
 /**
  * Find the province object of the given province name
@@ -64,10 +73,14 @@ toExport.findCountryByNameOrShortName = getCountryByNameOrShortName;
 function getProvinceByName(name, useAlias) {
   if (!_.isString(name) || !_.isArray(this.provinces)) return undefined;
 
-  return _.find(this.provinces, function (province) {
+  return _.find(this.provinces, function(province) {
     if (useAlias) {
-      return province.name.toUpperCase() === name.toUpperCase()
-        ||  _.find(province.alias, function (alias) (alias.toUpperCase() === name.toUpperCase()));
+      return (
+        province.name.toUpperCase() === name.toUpperCase() ||
+        _.find(province.alias, function(alias) {
+          return alias.toUpperCase() === name.toUpperCase();
+        })
+      );
     }
     return province.name.toUpperCase() === name.toUpperCase();
   });
@@ -83,26 +96,40 @@ function getProvinceByName(name, useAlias) {
 function getProvinceByNameOrShortName(name, useAlias) {
   if (!_.isString(name) || !_.isArray(this.provinces)) return undefined;
 
-  return _.find(this.provinces, function (province) {
+  return _.find(this.provinces, function(province) {
     if (useAlias) {
-      return province.name.toUpperCase() === name.toUpperCase()
-        || (province.short && province.short.toUpperCase() === name.toUpperCase())
-        ||  _.find(province.alias, function (alias) (alias.toUpperCase() === name.toUpperCase()));
+      return (
+        province.name.toUpperCase() === name.toUpperCase() ||
+        (province.short &&
+          province.short.toUpperCase() === name.toUpperCase()) ||
+        _.find(province.alias, function(alias) {
+          return alias.toUpperCase() === name.toUpperCase();
+        })
+      );
     }
-    return province.name.toUpperCase() === name.toUpperCase() || (province.short && province.short.toUpperCase() === name.toUpperCase());
+    return (
+      province.name.toUpperCase() === name.toUpperCase() ||
+      (province.short && province.short.toUpperCase() === name.toUpperCase())
+    );
   });
 }
 
 /**
- * 
+ *
  * Add search function to each country and map each country by alpha2
  */
-const listCountries = _.keyBy(_.cloneDeep(countries), 'alpha2');
-_.forEach(listCountries, function (country, key) {
+const listCountries = _.keyBy(_.cloneDeep(countries), "alpha2");
+_.forEach(listCountries, function(country, key) {
   country.getProvinceByName = _.bind(getProvinceByName, country);
   country.findProvinceByName = _.bind(getProvinceByName, country);
-  country.getProvinceByNameOrShortName = _.bind(getProvinceByNameOrShortName, country);
-  country.findProvinceByNameOrShortName = _.bind(getProvinceByNameOrShortName, country);
+  country.getProvinceByNameOrShortName = _.bind(
+    getProvinceByNameOrShortName,
+    country
+  );
+  country.findProvinceByNameOrShortName = _.bind(
+    getProvinceByNameOrShortName,
+    country
+  );
   toExport[key] = country;
 });
 
